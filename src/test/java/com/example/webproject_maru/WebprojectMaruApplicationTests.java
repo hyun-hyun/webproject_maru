ed3c566df563e6dcdea0cc7e3d3079fc70673f75
정현서 <ox672@naver.com>
2024-03-12 00:06:39 +0900

    first commit

---
+++ src/test/java/com/example/webproject_maru/WebprojectMaruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webproject_maru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprojectMaruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
