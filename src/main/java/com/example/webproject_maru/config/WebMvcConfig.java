fa97f2c9ef28c6554d3afec17679f3b5955e3203
정현서 <ox672@naver.com>
2024-10-25 21:59:29 +0900

    [환경]docker배포->로컬테스트

--- src/main/java/com/example/webproject_maru/config/WebMvcConfig.java
+++ src/main/java/com/example/webproject_maru/config/WebMvcConfig.java
@@ -10,10 +10,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/**")
+
+                //------배포시 수정부분------
                 //로컬테스트 주소
-                //.addResourceLocations("file:src/main/resources/static/");
+                .addResourceLocations("file:src/main/resources/static/");
                 //배포 기준 주소
-                .addResourceLocations("file:/usr/local/tomcat/webapps/");
+                //.addResourceLocations("file:/usr/local/tomcat/webapps/");
 
         // registry.addResourceHandler("/**")
         //         .addResourceLocations("file:src/main/resources/templates/");
