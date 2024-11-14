e93bf856f54a6a8914aa0cc8e43c438f4ac78fa3
정현서 <ox672@naver.com>
2024-11-15 00:22:36 +0900

    댓글_api 보안 인증 권한 조정

--- src/main/java/com/example/webproject_maru/config/SpringSecurityConfig.java
+++ src/main/java/com/example/webproject_maru/config/SpringSecurityConfig.java
@@ -25,7 +25,7 @@ public class SpringSecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         //.requestMatchers("/", "/**").permitAll()
 
-                        .requestMatchers("/","/error", "/login", "/loginProc","/join","/joinProc","/joined","/articles/anime/**").permitAll()
+                        .requestMatchers("/","/error", "/login", "/loginProc","/join","/joinProc","/joined","/articles/anime/**","/api/comments/article/*").permitAll()
                         //h2용
                         //.requestMatchers("/h2-console/**").permitAll() 
 
@@ -34,8 +34,8 @@ public class SpringSecurityConfig {
                         .requestMatchers("/user/**").hasAnyRole("ADMIN", "MANAGER", "USER")
                         //.requestMatchers("/user/**", "/articles/**").hasAnyRole("ADMIN", "MANAGER", "USER")
                         .requestMatchers("/images/**","/css/**","/favicon.ico").permitAll()//이미지파일들 security filter 예외
-                        .requestMatchers("/api/articles/*/create_r", "/api/mypage/**").authenticated() // 특정 경로에 대해 인증 요구
-                        .requestMatchers("/api/reviews/**","/api/**").permitAll()
+                        .requestMatchers("/api/articles/*/create_r","/api/reviews/**", "/api/comments/**", "/api/mypage/**").authenticated() // 특정 경로에 대해 인증 요구
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
                 )
                 .requestCache(cache -> cache.requestCache(requestCache)) // 요청 캐시 설정(저 ?continue=가야할경로 그거)
